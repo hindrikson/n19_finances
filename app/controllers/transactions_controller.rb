e9f7f52aa952b2fd58bf814006ea8f93a4f2f329
Ruda Hindrikson <hindrikson@protonmail.com>
2026-04-10 22:55:14 +0200

    update

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,11 +8,26 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @transaction = Transaction.new(transaction_params)
-    if @transaction.save
-      redirect_to transactions_path, notice: "Transaction saved successfully."
+    if params[:payments].present?
+      params[:payments].each do |flatmate_id, data|
+        flatmate = Flatmate.find(flatmate_id)
+        Transaction.create!(
+          flatmate: flatmate.name,
+          transaction_type: "income",
+          date: data[:date],
+          amount: data[:amount],
+          room: flatmate.room,
+          description: "#{flatmate.name} rent"
+        )
+      end
+      redirect_to transactions_path, notice: "All rent payments saved successfully."
     else
-      render :new, status: :unprocessable_entity
+      @transaction = Transaction.new(transaction_params)
+      if @transaction.save
+        redirect_to transactions_path, notice: "Transaction saved successfully."
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
   end
 
