d86719a10a457202c786dda5eed70636d9638dbe
Ruda Hindrikson <hindrikson@proton.me>
2026-04-10 16:51:56 +0200

    update

--- lib/scripts/2026_04_transactions.rb
+++ lib/scripts/2026_04_transactions.rb
@@ -6,20 +6,19 @@ year = 2026
 income_date = Date.new(year, month, 10)
 expense_date = Date.new(year, month, 15)
 
-flatmates = Flatmate.all
 rooms = Room.all
 buffers = BufferCategories::CATEGORIES
 expenses = ExpenseCategories::CATEGORIES
 
-def create_rent_transacion(name, amount)
-  flatmate = flatmates.find_by(name: name)
+def create_rent_transacion(name, income_date, amount)
+  flatmate = Flatmate.all.find_by(name: name)
   if flatmate
     Transaction.create(
       flatmate: flatmate.name,
       transaction_type: "income",
       date: income_date,
       amount: amount,
-      room: flatmate.room.name,
+      room: flatmate.room,
       description: "#{flatmate.name} rent"
     )
   else
@@ -27,8 +26,8 @@ def create_rent_transacion(name, amount)
   end
 end
 
-def create_expense_transaction(name, amount, description)
-  if expenses.include?(name)
+def create_expense_transaction(name:, expense_date:, amount:, description:)
+  if ExpenseCategories::CATEGORIES.include?(name)
     Transaction.create(
       transaction_type: "expense",
       name: name,
@@ -39,6 +38,7 @@ def create_expense_transaction(name, amount, description)
     )
   else
     puts "Expense category #{name} not found."
+    puts "Available categories: #{ExpenseCategories::CATEGORIES.join(', ')}"
   end
 end
 
@@ -46,15 +46,15 @@ end
 # ================================
 # INCOME RENT TRANSACTIONS
 # ================================
-create_rent_transacion("Jonathan", 699.12)
-create_rent_transacion("Maren", 160.0)
-create_rent_transacion("Ruda", 626.62)
-create_rent_transacion("Arce", 437.81)
-create_rent_transacion("Nona", 522.67)
-create_rent_transacion("Tanja", 554.01)
-create_rent_transacion("Viola", 473.41)
-create_rent_transacion("Lisa", 354.14)
-create_rent_transacion("Ronny", 491.32)
+create_rent_transacion("Jonathan", income_date, 699.12)
+create_rent_transacion("Maren",    income_date, 160.0)
+create_rent_transacion("Ruda",     income_date, 626.62)
+create_rent_transacion("Arce",     income_date, 437.81)
+create_rent_transacion("Nona",     income_date, 522.67)
+create_rent_transacion("Tanja",    income_date, 554.01)
+create_rent_transacion("Viola",    income_date, 473.41)
+create_rent_transacion("Lisa",     income_date, 354.14)
+create_rent_transacion("Ronny",    income_date, 491.32)
 
 # ================================
 # OTHER INCOME TRANSACTIONS
@@ -67,48 +67,40 @@ Transaction.create(
   description: "Nona: deposit"
 )
 
-Transaction.create(
-  name: "electrcity payback",
-  transaction_type: "income",
-  category: "electricity_buffer",
-  date: income_date,
-  amount: 400,
-  description: "Qcells: payback"
-)
 
 # ================================
 # EXPENSES TRANSACTIONS
 # ================================
 # water (RheinEnergie)
-create_expense_transaction(name: "water_bill",
+create_expense_transaction(name: "water_bill", expense_date: expense_date,
                            amount: 49.0,
                            description: "RheinEnergie: monthly water bill")
 # internet (Telekom)
-create_expense_transaction(name: "internet_bill",
+create_expense_transaction(name: "internet_bill", expense_date: expense_date,
                            amount: 54.0,
                            description: "Maren: internet Telekom")
 # GEZ Rundfunk
-create_expense_transaction(name: "gez",
+create_expense_transaction(name: "gez", expense_date: expense_date,
                            amount: 55.08,
                            description: "Rundfunk (GEZ) fee")
 # wash machine (Jonathan: Leasing of the wash machine)
-create_expense_transaction(name: "wash_machine_bill",
+create_expense_transaction(name: "wash_machine_bill", expense_date: expense_date,
                            amount: 14.99,
                            description: "Jonathan: Leasing of the wash machine")
 # flat electricity (Jonathan: flat electricity (Naturstrom))
-create_expense_transaction(name: "flat_electricity_bill",
+create_expense_transaction(name: "flat_electricity_bill", expense_date: expense_date,
                            amount: 29.0,
                            description: "Jonathan: flat electricity (Naturstrom)")
 # haus electricity (Jonathan: house electricity (Qcells))
-create_expense_transaction(name: "house_electricity_bill",
+create_expense_transaction(name: "house_electricity_bill", expense_date: expense_date,
                            amount: 200.0,
                            description: "Jonathan: house electricity Qcells")
 # rent (rent: Kai und Dirk Hannheiser
-create_expense_transaction(name: "rent",
+create_expense_transaction(name: "rent", expense_date: expense_date,
                            amount: 3335.0,
                            description: "rent: Kai und Dirk Hahnheiser")
 # accout fees
-create_expense_transaction(name: "account_fees",
+create_expense_transaction(name: "account_fees", expense_date: expense_date,
                            amount: 3.80,
                            description: "account fees")
 
